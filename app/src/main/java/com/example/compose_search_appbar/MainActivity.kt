36ab3f86a9a3a56432d5656ac4bbecd3ba30fd45
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2023-07-07 22:11:09 +0900

    초안 완성

--- app/src/main/java/com/example/compose_search_appbar/MainActivity.kt
+++ app/src/main/java/com/example/compose_search_appbar/MainActivity.kt
@@ -1,53 +1,211 @@
 package com.example.compose_search_appbar
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ContentTransform
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActionScope
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.compose_search_appbar.ui.theme.ComposesearchappbarTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposesearchappbarTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                val focusManager = LocalFocusManager.current
+                Scaffold(topBar = { SearchAppBar() }
+                   ) {
+                    Column(modifier = Modifier
+                        .padding(it)
+                        .addFocusCleaner(focusManager = focusManager)
+                        .background(Color.Cyan)
+                        .height(300.dp)
+                        .fillMaxWidth()) {
+                    }
             }
         }
     }
 }
+    @OptIn(ExperimentalAnimationApi::class, ExperimentalMaterial3Api::class)
+    @Composable
+    fun SearchAppBar() {
+        val focusManager = LocalFocusManager.current
+        val context = LocalContext.current
+        val focusRequester by remember { mutableStateOf(FocusRequester()) }
+        var text by remember { mutableStateOf("") }
+        var showTextField by remember { mutableStateOf(false) }
+        TopAppBar(
+            modifier = Modifier.addFocusCleaner(focusManager = focusManager),
+            colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Blue),
+            actions = {
+                IconButton(onClick = {
+                    if (showTextField && text.isNotBlank() && text.isNotEmpty()) {
+                        Toast.makeText(context, "search start", Toast.LENGTH_SHORT).show()
+                        text = ""
+                    }
+                    showTextField = showTextField.not()
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search Icon",
+                    )
+                }
+            },
+            title = {
+                Row() {
+                    AnimatedContent(
+                        targetState = showTextField,
+                        transitionSpec = {
+                            if ( targetState > initialState ){
+                                ContentTransform(
+                                    targetContentEnter = slideInHorizontally { height -> height } + fadeIn(),
+                                    initialContentExit = slideOutHorizontally { height -> -height } + fadeOut()
+                                )
+                            } else {
+                                ContentTransform(
+                                    targetContentEnter = slideInHorizontally { height -> -height } + fadeIn(),
+                                    initialContentExit = slideOutHorizontally { height -> height } + fadeOut()
+                                )
+                            }
+                        }
+                    ) {isSearch ->
+                        if (isSearch) {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(.7f)
+                                    .fillMaxHeight(.05f)
+                                    .border(
+                                        border = BorderStroke(1.dp, Color.LightGray),
+                                        shape = RoundedCornerShape(24.dp)
+                                    )
+                                    .clip(shape = RoundedCornerShape(24.dp))
+                                    .background(Color.LightGray)
+                                ,
+
+                                ) {
+                                SearchTextField(
+                                    modifier = Modifier.weight(1f).align(CenterVertically),
+                                    query = text,
+                                    focusRequester = focusRequester,
+                                    onQueryChange = {
+                                        text = it
+                                    },
+                                    onQueryDone = {
+                                        if (showTextField && text.isNotBlank() && text.isNotEmpty()) {
+                                            Toast.makeText(context, "search start", Toast.LENGTH_SHORT).show()
+                                            text = ""
+                                        }
+                                        showTextField = showTextField.not()
+                                    }
+                                )
+                                IconButton(onClick = { text = "" }) {
+                                    Icon(
+                                        imageVector = Icons.Default.Close,
+                                        contentDescription = "Close Icon",
+                                    )
+                                }
+                            }
+                        } else {
+                            Text(text = "text")
+                        }
+                    }
+                }
+
+            })
+
+    }
+}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    SearchAppBar(onSearch = { /*TODO*/ }, onSearchTextChanged = { /*TODO*/ }) {
-        
+private fun SearchTextField(
+    modifier: Modifier = Modifier,
+    focusRequester: FocusRequester,
+    onQueryChange: (String) -> Unit,
+    onQueryDone: (KeyboardActionScope.() -> Unit)?,
+    query: String
+    ) {
+    BasicTextField(
+        value = query,
+        onValueChange = onQueryChange,
+        modifier = modifier
+            .padding(start = 10.dp)
+            .focusRequester(focusRequester = focusRequester),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(onDone = onQueryDone)
+    )
+}
+
+fun Modifier.addFocusCleaner(focusManager: FocusManager, doOnClear: () -> Unit = {}): Modifier {
+    return this.pointerInput(Unit) {
+        detectTapGestures(onTap = {
+            doOnClear()
+            focusManager.clearFocus()
+        })
     }
 }
--- app/src/main/java/com/example/compose_search_appbar/SearchAppBar.kt
+++ app/src/main/java/com/example/compose_search_appbar/SearchAppBar.kt
@@ -1,24 +1,41 @@
 package com.example.compose_search_appbar
 
+import android.util.Log
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ContentTransform
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.CubicBezierEasing
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
@@ -26,9 +43,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.LastBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
+import kotlin.math.max
+import kotlin.math.roundToInt
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalAnimationApi::class)
 @Composable
 fun SearchAppBar(
     title: String = "",
@@ -40,46 +66,182 @@ fun SearchAppBar(
 ) {
 
     var showTextField by remember { mutableStateOf(false) }
-    Row(modifier = modifier
-        .fillMaxWidth()
-        .background(color = Color.White)
-        .height(75.dp)
-        .padding(10.dp),) {
-        Row(
-            Modifier
-                .weight(1f)
-                .height(75.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            AnimatedVisibility(
-                visible = !showTextField,
-                enter = slideInHorizontally(animationSpec = tween(500)),
-                exit = slideOutHorizontally(animationSpec = tween(500))
+    Column(modifier = Modifier.fillMaxWidth().height(75.dp)) {
+        Row() {
+            AnimatedContent(
+                targetState = showTextField,
+                transitionSpec = {
+                    if ( targetState > initialState ){
+                        ContentTransform(
+                            targetContentEnter = slideInVertically { height -> height } + fadeIn(),
+                            initialContentExit = slideOutVertically { height -> -height } + fadeOut()
+                        )
+                    } else {
+                        ContentTransform(
+                            targetContentEnter = slideInVertically { height -> -height } + fadeIn(),
+                            initialContentExit = slideOutVertically { height -> height } + fadeOut()
+                        )
+                    }
+                }
             ) {
-                Text(modifier = Modifier.fillMaxSize(), text = "title of your app", color = Color.Black)
-            }
+                Log.d("test", "boolean : $it")
 
-            AnimatedVisibility(
-                visible = showTextField,
-                enter = slideInHorizontally(animationSpec = tween(500)),
-                exit = slideOutHorizontally(animationSpec = tween(500))
+                if (it) {
+                    Text(text = "text")
+                } else {
+                    OutlinedTextField(
+                        value = "text",
+                        onValueChange = {
+                        },
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                }
+
+            }
+//            IconButton(
+//                modifier = Modifier.size(75.dp), onClick = { showTextField = showTextField.not()}) {
+//                Icon(
+//                    imageVector = Icons.Default.Search,
+//                    contentDescription = "Search Icon",
+//                )
+//            }
+        }
+    }
+}
+
+@Composable
+private fun TopAppBarLayout(
+    modifier: Modifier,
+    heightPx: Float,
+    navigationIconContentColor: Color,
+    titleContentColor: Color,
+    actionIconContentColor: Color,
+    title: @Composable () -> Unit,
+    titleTextStyle: TextStyle,
+    titleAlpha: Float,
+    titleVerticalArrangement: Arrangement.Vertical,
+    titleHorizontalArrangement: Arrangement.Horizontal,
+    titleBottomPadding: Int,
+    hideTitleSemantics: Boolean,
+    navigationIcon: @Composable () -> Unit,
+    actions: @Composable () -> Unit,
+) {
+    Layout(
+        {
+            Box(
+                Modifier
+                    .layoutId("navigationIcon")
+                    .padding(start = TopAppBarHorizontalPadding)
             ) {
-//                TextField(modifier = Modifier.fillMaxSize(),value = "", onValueChange = {})
-                OutlinedTextField(
-                    value = "text",
-                    onValueChange = {
-                    },
-                    shape = RoundedCornerShape(12.dp)
+                CompositionLocalProvider(
+                    LocalContentColor provides navigationIconContentColor,
+                    content = navigationIcon
                 )
             }
+            Box(
+                Modifier
+                    .layoutId("title")
+                    .padding(horizontal = TopAppBarHorizontalPadding)
+                    .then(if (hideTitleSemantics) Modifier.clearAndSetSemantics { } else Modifier)
+            ) {
+                ProvideTextStyle(value = titleTextStyle) {
+                    CompositionLocalProvider(
+                        LocalContentColor provides titleContentColor.copy(alpha = titleAlpha),
+                        content = title
+                    )
+                }
+            }
+            Box(
+                Modifier
+                    .layoutId("actionIcons")
+                    .padding(end = TopAppBarHorizontalPadding)
+            ) {
+                CompositionLocalProvider(
+                    LocalContentColor provides actionIconContentColor,
+                    content = actions
+                )
+            }
+        },
+        modifier = modifier
+    ) { measurables, constraints ->
+        val navigationIconPlaceable =
+            measurables.first { it.layoutId == "navigationIcon" }
+                .measure(constraints.copy(minWidth = 0))
+        val actionIconsPlaceable =
+            measurables.first { it.layoutId == "actionIcons" }
+                .measure(constraints.copy(minWidth = 0))
+
+        val maxTitleWidth = if (constraints.maxWidth == Constraints.Infinity) {
+            constraints.maxWidth
+        } else {
+            (constraints.maxWidth - navigationIconPlaceable.width - actionIconsPlaceable.width)
+                .coerceAtLeast(0)
         }
-        IconButton(onClick = { showTextField = showTextField.not()}) {
-            Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = "Search Icon",
+        val titlePlaceable =
+            measurables.first { it.layoutId == "title" }
+                .measure(constraints.copy(minWidth = 0, maxWidth = maxTitleWidth))
+
+        // Locate the title's baseline.
+        val titleBaseline =
+            if (titlePlaceable[LastBaseline] != AlignmentLine.Unspecified) {
+                titlePlaceable[LastBaseline]
+            } else {
+                0
+            }
+
+        val layoutHeight = heightPx.roundToInt()
+
+        layout(constraints.maxWidth, layoutHeight) {
+            // Navigation icon
+            navigationIconPlaceable.placeRelative(
+                x = 0,
+                y = (layoutHeight - navigationIconPlaceable.height) / 2
+            )
+
+            // Title
+            titlePlaceable.placeRelative(
+                x = when (titleHorizontalArrangement) {
+                    Arrangement.Center -> (constraints.maxWidth - titlePlaceable.width) / 2
+                    Arrangement.End ->
+                        constraints.maxWidth - titlePlaceable.width - actionIconsPlaceable.width
+                    // Arrangement.Start.
+                    // An TopAppBarTitleInset will make sure the title is offset in case the
+                    // navigation icon is missing.
+                    else -> max(TopAppBarTitleInset.roundToPx(), navigationIconPlaceable.width)
+                },
+                y = when (titleVerticalArrangement) {
+                    Arrangement.Center -> (layoutHeight - titlePlaceable.height) / 2
+                    // Apply bottom padding from the title's baseline only when the Arrangement is
+                    // "Bottom".
+                    Arrangement.Bottom ->
+                        if (titleBottomPadding == 0) layoutHeight - titlePlaceable.height
+                        else layoutHeight - titlePlaceable.height - max(
+                            0,
+                            titleBottomPadding - titlePlaceable.height + titleBaseline
+                        )
+                    // Arrangement.Top
+                    else -> 0
+                }
+            )
+
+            // Action icons
+            actionIconsPlaceable.placeRelative(
+                x = constraints.maxWidth - actionIconsPlaceable.width,
+                y = (layoutHeight - actionIconsPlaceable.height) / 2
             )
         }
     }
-
 }
 
+// An easing function used to compute the alpha value that is applied to the top title part of a
+// Medium or Large app bar.
+/*@VisibleForTesting*/
+internal val TopTitleAlphaEasing = CubicBezierEasing(.8f, 0f, .8f, .15f)
+
+private val MediumTitleBottomPadding = 24.dp
+private val LargeTitleBottomPadding = 28.dp
+private val TopAppBarHorizontalPadding = 4.dp
+
+// A title inset when the App-Bar is a Medium or Large one. Also used to size a spacer when the
+// navigation icon is missing.
+private val TopAppBarTitleInset = 16.dp - TopAppBarHorizontalPadding
